d0cb83ddc1076c79f8607fb679e1f18104906e15
Blas Kojusner <bkojusner@ufl.edu>
2019-11-11 14:55:01 -0500

    Updated index file
    Edited html for search form

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
 import './Home.css';
 
 function Home() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                </a>
-            </header>
-        </div>
+        <body>
+            <h1>
+            Water Cleanliness by Zip Code
+            </h1>
+            <form action="search.php">
+            <fieldset>
+            <p>
+            <input name="search" type="text" placeholder="Enter Zip..." />
+            <button class="button" type="submit">
+            <span><b>Search</b></span>
+            </button>
+            </p>
+            </fieldset>
+            </form>   
+        </body>
     );
 }
 
