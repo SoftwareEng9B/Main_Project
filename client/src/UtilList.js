bc9ca538216552e17555c39842b85f7acf774f53
Daniel Bramwell <dbramwell@ufl.edu>
2019-11-12 14:56:35 -0500

    Fixed UI naming and link errors for utilieis -db

--- client/src/UtilList.js
+++ client/src/UtilList.js
@@ -7,12 +7,18 @@ function UtilList(props) {
 
 	function utilsContam(){
 		console.log(refer.current)
-		utilUpdate(refer.current.value);	
+		// utilUpdate(refer.current.value);	
 	}
 
 	const utilList = data.map(directory => {
 		return (
-			<input type="button" ref={refer} key={directory.link} value={directory.link} onClick={utilsContam}/>		
+			// <input type="button" ref={refer} key={directory.link} value={directory.name} onClick={utilsContam}/>
+			<tr key={directory.link}>
+					{/* <td>{directory.name} </td> */}
+					<td onClick={()=> utilUpdate(directory.link)}> 
+						{directory.name}
+					</td>
+			</tr>
 		);
 	});
 
@@ -24,7 +30,11 @@ function UtilList(props) {
 
 	return(
 		<div>
-			<div>{utilList}</div>
+			<h3>Utilies</h3>
+			<table>
+				<tbody>{utilList}</tbody>
+			</table>
+			<h3>Contaminents at selected Utility</h3>
 			{contamsList}
 		</div>
 
