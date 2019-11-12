8bf13e1f6ac1f755bb28d2c03ed9ed9f59fe6d59
lukinator1 <32145995+lukinator1@users.noreply.github.com>
2019-11-12 14:11:06 -0500

    backend receiving input from frontend's request

--- server/controllers/examples.server.controller.js
+++ server/controllers/examples.server.controller.js
@@ -4,11 +4,14 @@ const scraper = require("./scraper");
 
 exports.scrape = async function(req, res) {
     //TODO: input zip code from user/UI
-    let zip = '33325';
+    let zip = req.body.zipcode;
+    //let zip = '33325';
     const utilData = await scraper.getUtils(zip);
     //TODO: output/display utils to user/UI
+    //res.json(utilData);
     //TODO: input util selection from user/UI
-    let util = utilData.featuredUtil.link;
+    let util = req.body.utilselection;
+    //let util = utilData.featuredUtil.link;
     const contamData = await scraper.getContams(util)
     res.json(contamData);
-};
+    };
