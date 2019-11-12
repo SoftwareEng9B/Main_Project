58556306d9e464811f4e997dafec1f9db1e11aa7
Savvas <sferekides@yahoo.com>
2019-11-12 12:06:50 -0500

    Editted button functionality for utlities list

--- client/src/UtilList.js
+++ client/src/UtilList.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 function UtilList(props) {
-		
-	//console.log('This is my directory file', this.props.data);
 	const { data } = props;
-	//set building list = to only select files that you want to look at
 	const utilList = data.map(directory => {
 		return (
-			<tr key={directory.link}>
+			<button key={directory.link} >
 				<td>{directory.name} </td>
-			</tr>
+			</button>
+		
 		);
 	});
 
