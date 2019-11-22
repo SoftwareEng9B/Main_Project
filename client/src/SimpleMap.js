dade234e94026d3f12bf232e483b7a3a10ef1926
Daniel Bramwell <dbramwell@ufl.edu>
2019-11-22 01:36:08 -0500

    Created initial map feature w/ Jared. Map locates given zipcode

---
+++ client/src/SimpleMap.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+let AnyReactComponent = ({ text }) => (
+  <div style={{
+    color: 'white', 
+    background: 'grey',
+    padding: '15px 10px',
+    display: 'inline-flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    transform: 'translate(-50%, -50%)'
+  }}>
+    {text}
+  </div>
+);
+ 
+class SimpleMap extends Component {
+
+  render() {
+    if(this.props.zipCoords){
+      return (
+        // Important! Always set the container height explicitly
+        <div style={{ height: '50vh', width: '50%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{key: 'AIzaSyAw6nNDn_HeWc1PVvTcynfEufYbgTtv004'}}
+            defaultCenter={{
+              lat: 59.95,
+              lng: 30.33 
+            }}
+            defaultZoom={11}
+            center={this.props.zipCoords}
+          >
+            <AnyReactComponent
+              lat={this.props.zipCoords.lat}
+              lng={this.props.zipCoords.lng}
+              text={this.props.zipcode}
+            />
+          </GoogleMapReact>
+        </div>
+      );
+    }else{
+      return <div></div>;
+    }
+  }
+}
+ 
+export default SimpleMap;
